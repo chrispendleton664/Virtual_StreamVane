fa5d9157b4a8d14034b55fcf32c0c15b396e661d
Gerico Vidanes <gericov@outlook.com>
2020-11-13 17:37:05 +0000

    Set of test case for comparing invsicid and incompressible cases to literature
    Set of config files for recreating test cases described in K. Smith (2017)

---
+++ testcases/Vel_Profiles/Inviscid-Incompressible/recreate_testcases.bat
@@ -0,0 +1,5 @@
+python ..\..\..\swirlgenerator bulk_swirl.config -saveplots bulk_swirl.pdf -checkboundaries
+python ..\..\..\swirlgenerator twin_swirl.config -saveplots twin_swirl.pdf -checkboundaries
+python ..\..\..\swirlgenerator offset_swirl_1.config -saveplots offset_swirl_1.pdf -checkboundaries
+python ..\..\..\swirlgenerator offset_swirl_2.config -saveplots offset_swirl_2.pdf -checkboundaries
+PAUSE
